0e69cc3aa1bf2bfa147eb9eddcfab8810102dc36
Nicholas Semaan <nsemaan23@gmail.com>
2023-08-01 22:42:37 -0700

    fifth commit, share/post feature added for both description and img

--- api/index.js
+++ api/index.js
@@ -1,12 +1,13 @@
 import express from "express";
 const app = express()
-import authRoutes from "./routes/auth.js"
-import userRoutes from "./routes/users.js"
-import postRoutes from "./routes/posts.js"
-import commentRoutes from "./routes/comments.js"
-import likeRoutes from "./routes/likes.js"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import commentRoutes from "./routes/comments.js";
+import likeRoutes from "./routes/likes.js";
+import cors from "cors";
+import multer from "multer";
+import cookieParser from "cookie-parser";
 
 //middlewares
 app.use((req,res,next) =>{
@@ -20,6 +21,22 @@ app.use(cors({
 }))
 
 app.use(cookieParser())
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '../client2/public/upload');
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + file.originalname);
+    }
+});
+  
+const upload = multer({ storage: storage })
+
+app.post("/api/upload", upload.single("file"), (req, res) =>{
+    const file = req.file;
+    res.status(200).json(file.filename);
+});
 
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
