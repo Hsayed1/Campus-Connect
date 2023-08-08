63f609294ec9cab159c7bf77fadb5ea132b1eca5
Hamed Sayed <hamed.sayed@sjsu.edu>
2023-08-07 17:37:07 -0700

    more style changes to profile page

--- client2/src/pages/profile/Profile.jsx
+++ client2/src/pages/profile/Profile.jsx
@@ -1,13 +1,5 @@
 import React, { useState } from "react";
 import "./profile.scss";
-import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import PinterestIcon from "@mui/icons-material/Pinterest";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import PlaceIcon from "@mui/icons-material/Place";
-import LanguageIcon from "@mui/icons-material/Language";
-import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import Posts from "../../components/posts/Posts";
 import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
@@ -88,31 +80,15 @@ const Profile = () => {
       <div className="profileContainer">
         <div className="uInfo">
           <div className="left">
-            <a href="http://facebook.com">
-              <FacebookTwoToneIcon fontSize="large" />
-            </a>
-            <a href="http://facebook.com">
-              <InstagramIcon fontSize="large" />
-            </a>
-            <a href="http://facebook.com">
-              <TwitterIcon fontSize="large" />
-            </a>
-            <a href="http://facebook.com">
-              <LinkedInIcon fontSize="large" />
-            </a>
-            <a href="http://facebook.com">
-              <PinterestIcon fontSize="large" />
-            </a>
+      
           </div>
           <div className="center">
             <span>{data.name}</span>
             <div className="info">
               <div className="item">
-                <PlaceIcon />
                 <span>{data.city}</span>
               </div>
               <div className="item">
-                <LanguageIcon />
                 <span>{data.website}</span>
               </div>
             </div>
@@ -127,7 +103,6 @@ const Profile = () => {
             )}
           </div>
           <div className="right">
-            <EmailOutlinedIcon />
             <MoreVertIcon />
           </div>
         </div>
