b0c68d30d18cc893379170ec454003e283a000e5
Nicholas Semaan <nsemaan23@gmail.com>
2023-08-07 17:17:24 -0700

    fixed bugs related to profile

--- client2/src/pages/profile/Profile.jsx
+++ client2/src/pages/profile/Profile.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import "./profile.scss";
 import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
@@ -14,34 +15,36 @@ import { makeRequest } from "../../axios";
 import { useLocation } from "react-router-dom";
 import { AuthContext } from "../../context/authContext";
 import { useContext } from "react";
-import Update from "../../components/update/Update"
-import { useState } from "react";
+import Update from "../../components/update/Update";
 
 const Profile = () => {
-  const [openUpdate, setOpenUpdate] = useState(false)
+  const [openUpdate, setOpenUpdate] = useState(false);
   const { currentUser } = useContext(AuthContext);
   const userId = parseInt(useLocation().pathname.split("/")[2]);
 
-  const { isLoading, error, data } = useQuery(["user"], () =>
-    makeRequest.get("/users/find/" + userId).then((res) => {
-      return res.data;
-    })
-  );
-
-  const { isLoading: rIsLoading, data: relationshipData } = useQuery(
-    ["relationship"],
+  const { isLoading: userLoading, error: userError, data } = useQuery(
+    ["user"],
     () =>
-      makeRequest.get("/relationships?followedUserId=" + userId).then((res) => {
+      makeRequest.get("/users/find/" + userId).then((res) => {
         return res.data;
       })
+  );
+
+  const { isLoading: relationshipLoading, data: relationshipData } = useQuery(
+    ["relationship"],
+    () =>
+      makeRequest
+        .get("/relationships?followedUserId=" + userId)
+        .then((res) => {
+          return res.data;
+        })
   );
 
   const queryClient = useQueryClient();
 
   const mutation = useMutation(
     (following) => {
-      if (following)
-        return makeRequest.delete("/relationships?userId=" + userId);
+      if (following) return makeRequest.delete("/relationships?userId=" + userId);
 
       return makeRequest.post("/relationship", { userId });
     },
@@ -56,69 +59,81 @@ const Profile = () => {
     mutation.mutate(relationshipData.includes(currentUser.id));
   };
 
+  if (userLoading || relationshipLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (userError) {
+    return <div>Error occurred while fetching user data.</div>;
+  }
+
+  if (!data) {
+    return <div>User data not found.</div>;
+  }
+
   return (
     <div className="profile">
-      {isLoading ? (
-        "loading"
-      ) : (
-        <>
-          <div className="images">
-            <img src={"/upload/"+data.coverPic} alt="" className="cover" />
-            <img src={"/upload/"+data.profilePic} alt="" className="profilePic" />
+      <div className="images">
+        <img
+          src={data.coverPic ? "/upload/" + data.coverPic : ""}
+          alt=""
+          className="cover"
+        />
+        <img
+          src={data.profilePic ? "/upload/" + data.profilePic : ""}
+          alt=""
+          className="profilePic"
+        />
+      </div>
+      <div className="profileContainer">
+        <div className="uInfo">
+          <div className="left">
+            <a href="http://facebook.com">
+              <FacebookTwoToneIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <InstagramIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <TwitterIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <LinkedInIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <PinterestIcon fontSize="large" />
+            </a>
           </div>
-          <div className="profileContainer">
-            <div className="uInfo">
-              <div className="left">
-                <a href="http://facebook.com">
-                  <FacebookTwoToneIcon fontSize="large" />
-                </a>
-                <a href="http://facebook.com">
-                  <InstagramIcon fontSize="large" />
-                </a>
-                <a href="http://facebook.com">
-                  <TwitterIcon fontSize="large" />
-                </a>
-                <a href="http://facebook.com">
-                  <LinkedInIcon fontSize="large" />
-                </a>
-                <a href="http://facebook.com">
-                  <PinterestIcon fontSize="large" />
-                </a>
+          <div className="center">
+            <span>{data.name}</span>
+            <div className="info">
+              <div className="item">
+                <PlaceIcon />
+                <span>{data.city}</span>
               </div>
-              <div className="center">
-                <span>{data.name}</span>
-                <div className="info">
-                  <div className="item">
-                    <PlaceIcon />
-                    <span>{data.city}</span>
-                  </div>
-                  <div className="item">
-                    <LanguageIcon />
-                    <span>data.website</span>
-                  </div>
-                </div>
-                {rIsLoading ? (
-                  "loading"
-                ) : userId === currentUser.id ? (
-                  <button onClick={()=>setOpenUpdate(true)}>update</button>
-                ) : (
-                  <button onClick={handleFollow}>
-                    {relationshipData.includes(currentUser.id)
-                      ? "Following"
-                      : "Follow"}
-                  </button>
-                )}
-              </div>
-              <div className="right">
-                <EmailOutlinedIcon />
-                <MoreVertIcon />
+              <div className="item">
+                <LanguageIcon />
+                <span>{data.website}</span>
               </div>
             </div>
-            <Posts userId={userId}/>
+            {userId === currentUser.id ? (
+              <button onClick={() => setOpenUpdate(true)}>Update</button>
+            ) : (
+              <button onClick={handleFollow}>
+                {relationshipData.includes(currentUser.id)
+                  ? "Following"
+                  : "Follow"}
+              </button>
+            )}
           </div>
-        </>
-      )}
-      {openUpdate && <Update setOpenUpdate={setOpenUpdate} user={data}/>}
+          <div className="right">
+            <EmailOutlinedIcon />
+            <MoreVertIcon />
+          </div>
+        </div>
+        <Posts userId={userId} />
+      </div>
+      {openUpdate && <Update setOpenUpdate={setOpenUpdate} user={data} />}
     </div>
   );
 };
--- client2/src/pages/register/Register.jsx
+++ client2/src/pages/register/Register.jsx
@@ -1,35 +1,32 @@
 import { Link } from "react-router-dom";
 import "./register.scss";
-import {useState} from 'react';
+import { useState } from "react";
 import axios from "axios";
 
 const Register = () => {
   const [inputs, setInputs] = useState({
-    username:"",
-    email:"",
-    password:"",
-    name:"",
+    username: "",
+    email: "",
+    password: "",
+    name: "",
   });
 
   const [err, setErr] = useState(null);
 
-  const handleChange = e =>{
+  const handleChange = (e) => {
     setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-  }
-  
-  const handleClick = async e => {
-    e.preventDefault()
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
 
     try {
-      await axios.post("http://localhost:8800/api/auth/register", inputs)
-    } catch(err) {
-        console.log(err.response.data);
-        setErr (err);
+      await axios.post("http://localhost:8800/api/auth/register", inputs);
+    } catch (err) {
+      console.log(err.response.data);
+      setErr(err.response.data); // Use err.response.data instead of just err
     }
-
-  }
-
-
+  };
 
   return (
     <div className="register">
@@ -37,22 +34,22 @@ const Register = () => {
         <div className="left">
           <h1></h1>
           <p>
-           Get ready to embark on a journey of friendships, adventures, and unforgettable experiences!
+            Get ready to embark on a journey of friendships, adventures, and unforgettable experiences!
           </p>
           <span>Do you have an account?</span>
           <Link to="/login">
-          <button>Login</button>
+            <button>Login</button>
           </Link>
         </div>
         <div className="right">
           <h1>Register</h1>
           <form>
-            <input type="text" placeholder="Username" name="username" onChange={handleChange}/>
-            <input type="email" placeholder="Email" name="email" onChange={handleChange}/>
-            <input type="password" placeholder="Password" name="password" onChange={handleChange}/>
-            <input type="text" placeholder="Name" name="name" onChange={handleChange}/>
-            {err & err}
-            <button onClick = {handleClick}>Register</button>
+            <input type="text" placeholder="Username" name="username" onChange={handleChange} />
+            <input type="email" placeholder="Email" name="email" onChange={handleChange} />
+            <input type="password" placeholder="Password" name="password" onChange={handleChange} />
+            <input type="text" placeholder="Name" name="name" onChange={handleChange} />
+            {err && <div className="error">{err.message}</div>} {/* Render the error message */}
+            <button onClick={handleClick}>Register</button>
           </form>
         </div>
       </div>
